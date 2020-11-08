c64bccc8852eb2c66302916206bad8376073f9be
HappyFeed <alejomam2011@gmail.com>
2020-11-08 14:33:26 -0500

    arreglos primer paso y primeros acercamientos al resto de pasos

--- src/model/CYK.java
+++ src/model/CYK.java
@@ -3,25 +3,35 @@ package model;
 public class CYK {
 		
 	String[][] matriz;
-	
+	String[][]gramatica;
 	public CYK() {
 		
 	}
 	
 	public String algoritmoCYK(String[][] gramatica, String[] cadena) {
-		matriz= new String[cadena.length][cadena.length];
-		for (int j = 0; j < matriz.length; j++) {
-			matriz[j][0]=cadena[j];
-			matriz[0][j]=j+"";
+		matriz= new String[cadena.length+1][cadena.length+1];
+		this.gramatica=gramatica;
+		for (int j = 0; j < cadena.length; j++) {
+			matriz[j+1][0]=cadena[j];
+			matriz[0][j+1]=(j+1)+"";
 		}
 		
 		for (int j = 1; j < matriz.length; j++) {
 			for (int i = 1; i < matriz[j].length; i++) {
 				if(i==1) {
-					matriz[j][i]=primerPaso(matriz[j][i-1],gramatica);
+					matriz[j][i]=buscar(matriz[j][i-1]);	
 				}
+							
 			}
 		}
+		calcularBusqueda(2,cadena.length);
+		
+		/*for (int j = 1; j < matriz.length; j++) {
+			for (int i = 2; i < matriz.length; i++) {
+				calcularBusqueda(i,cadena.length);
+			}
+		}*/
+		
 		for (int x=0; x < matriz.length; x++) {
 			  System.out.print("|");
 			  for (int y=0; y < matriz[x].length; y++) {
@@ -33,16 +43,34 @@ public class CYK {
 		return null;
 	}
 	
-	public String primerPaso(String a, String[][] gramatica) {
-		String generadores="{";
+	public String buscar(String a) {
+		String generadores="";
 		for (int i = 0; i < gramatica.length; i++) {
 			for (int j = 0; j < gramatica[i].length; j++) {
 				if(gramatica[i][j].contentEquals(a)) {
-					generadores +=gramatica[i][0]+",";
+					generadores +=gramatica[i][0]+"";
 				}
 			}			
 		}
-		return generadores+"}";
+		if(generadores.equals("")) {
+			return "vacio";
+		}else {
+			return generadores;
+		}
+		
+	}
+	
+	
+	public void calcularBusqueda(int j, int n) {
+		for (int i = 1; i <= (n-j+1); i++) {
+			String caso="";
+			int p=0;
+			for (int k = 1; k <=(j-1); k++) {
+				caso=matriz[i][k]+matriz[i+k][j-k];
+				System.out.println(caso);
+			}
+			matriz[i][j]=buscar(caso);
+		}
 	}
 	
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -9,18 +9,21 @@ public class Main {
 	
 	public static void main(String[] args) {
 		flag= new CYK();
-		String line ="aabb";
+		String line ="bbab";
 		String[] parts= line.split("");
-		String[][] m= new String[3][3];
+		String[][] m= new String[4][3];
 		m[0][0]= "S";
 		m[1][0]= "A";
 		m[2][0]= "B";
+		m[3][0]= "C";
 		m[0][1]= "BA";
-		m[0][2]= "a";
-		m[1][1]= "B";
-		m[1][2]= "a";
-		m[2][1]= "A";
-		m[2][2]= "b";
+		m[0][2]= "AC";
+		m[1][1]= "CC";
+		m[1][2]= "b";
+		m[2][1]= "AB";
+		m[2][2]= "a";
+		m[3][1]= "BA";
+		m[3][2]= "a";
 		flag.algoritmoCYK(m, parts );
 	}
 
